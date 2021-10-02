139c1eb7fc26d9e676603b7576c7ac314483da78
Muhammad Bilal <mbilal2019@namal.edu.pk>
2021-10-02 20:24:45 +0500

    Add Functionality

---
+++ cal03.py
@@ -0,0 +1,9 @@
+#calculator in Python(v0.2)
+def add(num1,num2):
+	return num1+num2
+def div(num1,num2):
+	return num1/num2
+def mul(num1, num2):
+	return num1 * num2
+# main
+print(add(4,5),div(4,2),mul (6,4))
